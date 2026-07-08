c31b60038447d0c573fba08648957cc5e497b61d
jucatoruI <jucatorul.w@gmail.com>
2026-07-08 20:54:29 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
 const photo = document.querySelector(".photo");
 
 //doesn't rotate on y yet
-function dragonY(){
-    alert("AHHHH, stop that, I'm gonna throw up");
-}
+//function dragonY(){
+  //  alert("AHHHH, stop that, I'm gonna throw up");
+//}
 
-photo.addEventListener("mousedown", dragonY);
+//photo.addEventListener("mousedown", dragonY);
 
 //not working 
 //function enlarge(){}
